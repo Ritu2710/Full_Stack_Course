53cd8de5ac2c0faf88b83ad0e5e67d6b13da5175
Ritu <27ritu.patel10@gmail.com>
2021-10-02 22:21:36 +0530

    Primitive datatype

---
+++ app.js
@@ -0,0 +1,16 @@
+//primitive datatype
+//string
+console.log(typeof"hellow world")
+console.log(typeof"p")
+
+//number
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 11234659876809724356587823)
+console.log(typeof -1)
+console.log(typeof -1.2)
+console.log(typeof 11223344556677888)
+
+//boolean
+console.log(typeof true)
+console.log(typeof false)
