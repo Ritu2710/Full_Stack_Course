ad809b3a5f18d841eafa052f4f4fa1472c7cd579
Ritu <27ritu.patel10@gmail.com>
2021-10-05 22:38:31 +0530

    callback function

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
-// function greet(Name='riya',age='45'){
-//     console.log(Name,age)
-//     // console.log('Hello world')
-//     // console.log('Hey ' + Name + " Your age is " +age)
+var arr = ['go to the GYM', 'Eat Food', "Clean House"]
+
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
 // }
-// greet()
-
-
- function add(a = 0, b = 0) {
-     console.log('declaration')
-        var sum = a + b
-        return sum
- }
-
-
- console.log(add(10, 20))
 
 
 
-    var add = function (a = 0, b = 0) {
-        console.log('expression')
-        var sum = a + b
-        return sum
-    }
+// var callbackFunction = (element, index) => {
+//     console.log(element, index)
+// }
 
-    console.log(add(10, 20))
-
-//arrow function
+// arr.forEach(callbackFunction)
 
 
-    const greet = name => 'h1 ' + name
-
-
-
-    console.log(greet('Prabakaram'))
-
+arr.forEach((element, index) => {
+    console.log(element, index)
+})
